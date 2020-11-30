10cb101ffb9f1f9e57665529d43f034de78c70d7
Andrei Bessmertnyi <andreejbessmertnyj94@gmail.com>
2020-11-30 11:49:08 +0300

    store update

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import tasksReducer from './reducers/tasksSlice';
 import filtersReducer from './reducers/filtersSlice';
 import actionsReducer from './reducers/actionsSlice';
 
-export default configureStore({
-  reducer: {
-    tasks: tasksReducer,
-    filters: filtersReducer,
-    actions: actionsReducer,
-  },
+const reducer = combineReducers({
+  tasks: tasksReducer,
+  filters: filtersReducer,
+  actions: actionsReducer,
 });
+
+export default createStore(reducer, applyMiddleware(thunk));
